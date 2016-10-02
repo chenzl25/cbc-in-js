26bf284fb2e6a5a614191aadcfe943a4d0e4e279
dylan <chenzl25@mail2.sysu.edu.cn>
2016-10-02 14:42:20 +0800

    finish global common-subexpression-elimination

--- visitor/Optimizer/CommonSubExpression.js
+++ visitor/Optimizer/CommonSubExpression.js
@@ -28,13 +28,13 @@ CommonSubExpression.prototype = {
   cseDefinedFunction: function(bbs) {
     var EVAL = [];
     for (var i = 0; i < bbs._bbs.length; i++) {
-      var AEB = this.cseLocal(bbs.block(i));
+      var AEB = this.cseLocal(bbs.block(i), i);
       EVAL.push(this.cloneSet(AEB));
     }
     this.cseGlobal(bbs, EVAL);
   },
 
-  cseLocal: function (block) {
+  cseLocal: function (block, blockIndex) {
     var AEB = new Set; // Set {<pos, opd1, opr, opd2, tmp>}
     for (var i = 0; i < block.length(); i++) {
       var inst = block.inst(i);
@@ -52,8 +52,8 @@ CommonSubExpression.prototype = {
               ((this.opdValueEqual(this.opdValue(inst.from().left()), aeb.opd1) && 
                 this.opdValueEqual(this.opdValue(inst.from().right()), aeb.opd2)) ||
                (ir.Op.isComm(inst.from().op()) &&
-                inst.from().right().name() === aeb.opd1 && 
-                inst.from().left().name() === aeb.opd2))) {
+                this.opdValueEqual(this.opdValue(inst.from().right()), aeb.opd1) && 
+                this.opdValueEqual(this.opdValue(inst.from().left()), aeb.opd2)))) {
             var pos = aeb.pos;
             found = true;
             // if no variable in tuple, create a new temporary and
@@ -63,7 +63,8 @@ CommonSubExpression.prototype = {
             if (aeb.tmp === null) {
               var ti = ir.Reg.tmp();
               AEB.delete(aeb);
-              AEB.add({pos: aeb.pos, 
+              AEB.add({blk: blockIndex,
+                       pos: aeb.pos, 
                        opd1: aeb.opd1,
                        opr:  aeb.opr,
                        opd2: aeb.opd2,
@@ -88,7 +89,8 @@ CommonSubExpression.prototype = {
           }
         }
         if (!found) {
-          AEB.add({pos: i, 
+          AEB.add({blk: blockIndex,
+                   pos: i, 
                    opd1: this.opdValue(inst.from().left()),
                    opr:  inst.from().op(),
                    opd2: this.opdValue(inst.from().right()),
@@ -136,19 +138,18 @@ CommonSubExpression.prototype = {
   },
 
   cseGlobal: function(bbs, EVAL) {
-    // EVAL, KILL [Set {<opd1, opr, opd2>}]
+    // EVAL, KILL [Set {<blk, pos, opd1, opr, opd2>}]
     // AEin(i) = interset(AEout(j)) j ∈ Pred(i)
     // AEout(i) = EVAL(i) U (AEin(i) - KILL(i))
     // U = union(EVAL(i)) i ∈ all
 
     // -------------------------------
     // calU and recalEVAL want to make reference unique
-    // EVAL[0] = { <'a', '+', 'b'> } 
-    // EVAL[1] = { <'a', '+', 'b'> }
-    // U       = { <'a', '+', 'b'> }
-    // let <'a', '+', 'b'> be the same object 
+    // EVAL[0] = { <blk, pos, 'a', '+', 'b'> } 
+    // EVAL[1] = { <blk, pos, 'a', '+', 'b'> }
+    // U       = { <blk, pos, 'a', '+', 'b'> }
+    // let <blk, pos, 'a', '+', 'b'> be the same object 
     var U = this.calU(EVAL);
-    EVAL = this.recalEVAL(EVAL, U);
     // -------------------------------
     
     var KILL = this.calKILL(bbs, U, EVAL);
@@ -203,40 +204,75 @@ CommonSubExpression.prototype = {
           }
         }
       }
-    }
+    } // end of while
+    // console.log(U);
     // console.log(EVAL);
     // console.log(KILL);
-    console.log(AEin)
+    // console.log(AEin);
+    // console.log(AEout);
+
+    // apply the analysis
+    for (var i = 0; i < bbs._bbs.length; i++) {
+      var block = bbs.block(i);
+      for (var aeb of AEin[i]) {
+        for (var j = 0; j < block.length(); j++) {
+          var inst = block.inst(j);
+          if (inst instanceof ir.Move && 
+              inst.from() instanceof ir.Bin &&
+              (inst.from().left() instanceof ir.Reg || inst.from().left() instanceof ir.Int) && 
+              (inst.from().right() instanceof ir.Reg || inst.from().right() instanceof ir.Int) &&
+              inst.to() instanceof ir.Reg &&
+              inst.from().op() === aeb.opr &&
+              ((this.opdValueEqual(this.opdValue(inst.from().left()), aeb.opd1) && 
+                this.opdValueEqual(this.opdValue(inst.from().right()), aeb.opd2)) ||
+               (ir.Op.isComm(inst.from().op()) &&
+                this.opdValueEqual(this.opdValue(inst.from().right()), aeb.opd1) && 
+                this.opdValueEqual(this.opdValue(inst.from().left()), aeb.opd2)))) {
+              for (var k = j-1; k > 0; k--) {
+                if (block.insts(k) instanceof ir.Move &&
+                    (this.opdValueEqual(this.opdValue(block.insts(k).left()), aeb.opd1) ||
+                     this.opdValueEqual(this.opdValue(block.insts(k).left()), aeb.opd2))) {
+                  break;
+                }
+              }
+              var tj = ir.Reg.tmp();
+              // replace
+              inst._from = tj;
+              for (var item of AEin[i]) {
+                if (aeb.opd1 === item.opd1 && 
+                    aeb.opr === item.opr && 
+                    aeb.opd2 === item.opd2) {
+                  if (item.hasReplace !== true) {
+                    var lr = bbs.block(item.blk).inst(item.pos).to();
+                    bbs.block(item.blk).inst(item.pos)._to = new ir.Reg(tj.name());
+                    bbs.block(item.blk).insertAfter(item.pos, new ir.Move(null, new ir.Reg(tj.name()), lr));
+                    // renumber
+                    item.hasReplace = true;  
+                    this.reNumber(EVAL[item.blk], item.pos+1);
+                  } else {
+                    inst._from = new ir.Reg(bbs.block(item.blk).inst(item.pos).to().name());
+                  }
+                }
+              }
+              for (var item of EVAL[i]) {
+                if (item.pos === j) {
+                  item.hasReplace = true;
+                }
+              }
+          }
+        }        
+      }
+    }
   },
 
   calU: function(EVAL) {
     var U = new Set;
-    var visit = new Set;// Set {String} like 'a + b'
     for (var i = 0; i < EVAL.length; i++) {
       for (var aeb of EVAL[i]) {
-        if (!visit.has(aeb.opd1.toString() + aeb.opr.toString() + aeb.opd2.toString())) { 
-          U.add({opd1: aeb.opd1, opr:  aeb.opr, opd2: aeb.opd2});
-          visit.add(aeb.opd1.toString() + aeb.opr.toString() + aeb.opd2.toString());
-        }
+        U.add(aeb);
       }
     }
     return U;
-  },
-
-  recalEVAL: function(EVAL, U) {
-    for (var i = 0; i < EVAL.length; i++) {
-      var tmpSet = new Set;
-      for (var aeb of EVAL[i]) {
-        for (var item of U) {
-          if (aeb.opd1 === item.opd1 && aeb.opr === item.opr && aeb.opd2 === item.opd2) {
-            tmpSet.add(item);
-            break;
-          }
-        }
-      }
-      EVAL[i] = tmpSet;
-    }
-    return EVAL;
   },
  
   calKILL: function(bbs, U, EVAL) {
@@ -244,17 +280,15 @@ CommonSubExpression.prototype = {
     for (var i = 0; i < bbs._bbs.length; i++) {
       KILL.push(new Set);
       var block = bbs.block(i);
-      var visit = new Set; // Set {String} like 'a + b'
       for (var j = 0; j < block.length(); j++) {
         var inst = block.inst(j);
         if (inst instanceof ir.Move) {
           // this only work on : inst.to() is ir.Reg
           for (var aeb of U) {
-            if (this.opdValueEqual(aeb.opd1, this.opdValue(inst.to())) ||
-                this.opdValueEqual(aeb.opd2, this.opdValue(inst.to())) &&
-                !visit.has(aeb.opd1.toString() + aeb.opr.toString() + aeb.opd2.toString())) {
-              KILL[i].add({opd1: aeb.opd1, opr: aeb.opr, opd2: aeb.opd2});
-              visit.add(aeb.opd1.toString() + aeb.opr.toString() + aeb.opd2.toString());
+            if ((this.opdValueEqual(aeb.opd1, this.opdValue(inst.to())) ||
+                 this.opdValueEqual(aeb.opd2, this.opdValue(inst.to()))) &&
+                aeb.blk !== i) {
+              KILL[i].add(aeb);
             }
           }
         }
@@ -266,27 +300,12 @@ CommonSubExpression.prototype = {
   cloneSet:  function(s) {
     var result = new Set;
     for (var aeb of s) {
-      result.add({opd1: aeb.opd1, opr: aeb.opr, opd2: aeb.opd2});
+      result.add({blk: aeb.blk, 
+                  pos: aeb.pos, 
+                  opd1: aeb.opd1, 
+                  opr: aeb.opr, 
+                  opd2: aeb.opd2});
     }
     return result;
-  },
-
-  minusSet: function(s1,s2) {
-    var result = new Set;
-    for (var aeb of s1) {
-      if (!this.aebInSet(aeb, s2)) {
-        result.add({opd1: aeb.opd1, opr: aeb.opr, opd2: aeb.opd2});
-      }
-    }
-    return result;
-  },
-
-  aebInSet: function(aeb, s) {
-    for (var item of s) {
-      if (aeb.opd1 === item.opd1 && aeb.opr === item.opr && aeb.opd2 === item.opd2) {
-        return true
-      }
-    }
-    return false;
   }
 };
